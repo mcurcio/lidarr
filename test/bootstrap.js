4ceab897e15856402435b29a567dd69ed067eeb5
Matt Curcio <github@mcurcio.com>
2017-06-16 15:37:01 -0700

    updates

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,7 +1,15 @@
 'use strict';
 
+// set up chai/assert
 const chai = require('chai');
-
 chai.use(require('chai-as-promised'));
-
 global.assert = chai.assert;
+
+// add utility path functions
+const path = require('path');
+
+const FIXTURE_PATH = "fixtures";
+
+global.fixture = file => {
+	return path.join(__dirname, FIXTURE_PATH, file);
+};
--- test/unit/image.js
+++ test/unit/image.js
@@ -5,30 +5,24 @@ const path = require('path');
 
 const image = require('../../lib/image.js');
 
-const jpg = path.join(__dirname, '..', 'fixtures', '6132942800_3edd54eafc_o.jpg');
+const jpg = fixture('6132942800_3edd54eafc_o.jpg');
 
 describe('image', () => {
 	it('should exist', () => {
 		assert.ok(image);
 	});
 
-	describe('type', () => {
+	describe('stats', () => {
 		it('should work on images', () => {
-			return assert.becomes(image(jpg).type(), 'jpg');
+			return assert.eventually.include(image(jpg).stats(), {
+				type: 'jpg',
+				width: 1024,
+				height: 1024
+			});
 		});
 
 		it('should reject non-image', () => {
-			return assert.isRejected(image(__filename).type());
-		});
-	});
-
-	describe('size', () => {
-		it('should report size', () => {
-			return assert.eventually.deepEqual(image(jpg).size(), {width: 1024, height: 1024});
-		});
-
-		it('should fail on non-image', () => {
-			return assert.isRejected(image(__filename).type());
+			return assert.becomes(image(__filename).stats(), null);
 		});
 	});
 });
