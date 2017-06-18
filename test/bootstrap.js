8cf1361efb18cd91b8950dd8d696ca2be40b3168
Matt Curcio <github@mcurcio.com>
2017-06-18 15:59:14 -0700

    put test database in tmp dir

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,15 +1,38 @@
 'use strict';
 
-// set up chai/assert
+const fs = require('fs');
+const path = require('path');
+
 const chai = require('chai');
+const rimraf = require('rimraf');
+
+// set up chai/assert
 chai.use(require('chai-as-promised'));
 global.assert = chai.assert;
 
 // add utility path functions
-const path = require('path');
-
 const FIXTURE_PATH = "fixtures";
 
 global.fixture = file => {
 	return path.join(__dirname, FIXTURE_PATH, file);
+};
+
+global.lib = file => {
+	return path.join(__dirname, '..', 'lib', file);
+};
+
+global.makeTempDirectory = () => {
+	const tmpDirectory = path.join(__dirname, 'tmp');
+
+	return new Promise((resolve, reject) => {
+		rimraf(tmpDirectory, (err) => {
+			if (err) return reject(err);
+
+			fs.mkdir(tmpDirectory, (err) => {
+				if (err) return reject();
+
+				resolve(tmpDirectory);
+			});
+		});
+	});
 };
--- test/unit/db.js
+++ test/unit/db.js
@@ -1,10 +1,18 @@
 'use strict';
 
-const db = require('../../lib/db.js');
+const path = require('path');
+
+const database = require(lib('db'));
+
+let TMP;
 
 describe('database', () => {
+	beforeEach(() => {
+		return makeTempDirectory().then(dir => { TMP = dir; });
+	});
+
 	it('works', () => {
-		return require('../../lib/db.js')('./db.sqlite').then((db) =>{
+		return database(path.join(TMP, 'db.sqlite')).then((db) =>{
 			let camera = new db.Camera;
 			camera.name = 'Camera';
 			return assert.isFulfilled(camera.save());
