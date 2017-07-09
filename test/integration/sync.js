11515b9c3fd796f2fa047446f6c43c045c0a7def
Matt Curcio <github@mcurcio.com>
2017-07-09 12:17:28 -0700

    moments & relatives

--- test/integration/sync.js
+++ test/integration/sync.js
@@ -6,20 +6,18 @@ const path = require('path');
 const database = require(libPath('db'));
 const sync = require(libPath('sync'));
 
-function setupFixtures() {
-	let db;
-	return destroyTempDirectory()
-		.then(() => Promise.all([
-			makeTempDirectory('library'),
-			makeTempDirectory('imports')
-		]))
-		.then(() => fse.copy(FIXTURE_PATH, tmpPath('imports')))
-		.then(() => database(tmpPath('db.sqlite')))
-		.then(db_ => {
-			db = db_;
-			return db.migrator.up();
-		})
-		.then(() => db);
+async function setupFixtures() {
+	await  destroyTempDirectory();
+	await Promise.all([
+		makeTempDirectory('library'),
+		makeTempDirectory('imports')
+	]);
+	let db = await database(tmpPath('db.sqlite'));
+	await Promise.all([
+		fse.copy(FIXTURE_PATH, tmpPath('imports')),
+		db.migrator.up()
+	]);
+	return db;
 }
 
 describe('import', () => {
@@ -28,34 +26,43 @@ describe('import', () => {
 	});
 
 	describe('importer', () => {
-		let db;
-		it('should work', () => {
-			return setupFixtures()
-			.then((db_) => {
-				db = db_;
-				return sync.dir(tmpPath('imports'), tmpPath('library'), db).promise;
-			}).then(() => {
-				return Promise.all([
-					db.Photo.count(),
-					db.Location.count()
-				]);
-			}).then(([photoCount, locationCount]) => {
-				assert.equal(photoCount, 12);
-				assert.equal(locationCount, 12);
+		it('should work', async () => {
+			let db = await setupFixtures();
+			const LIB_DIR = tmpPath('library');
+			await sync.dir(tmpPath('imports'), LIB_DIR, db, {move:true}).promise;
+			let [photoCount, locationCount, relativeCount, momentCount] = await Promise.all([
+				db.Photo.count(),
+				db.Location.count(),
+				db.Relative.count(),
+				db.Moment.count()
+			]);
+			assert.equal(photoCount, 16);
+			assert.equal(locationCount, 16);
+			assert.equal(relativeCount, 1);
+			assert.equal(momentCount, 10);
+
+			let [locations, relatives] = await Promise.all([
+				db.Location.all(),
+				db.Relative.all()
+			]);
+			let promises = locations.map(location => {
+				let p = path.join(LIB_DIR, location.path);
+				return assert.becomes(fse.pathExists(p), true);
 			});
+			promises.concat(relatives.map(relative => {
+				let p = path.join(LIB_DIR, relative.path);
+				return assert.becomes(fse.pathExists(p), true);
+			}));
+			await Promise.all(promises);
 		});
 
-		it('should support cancellation', () => {
-			let db;
-			return setupFixtures()
-				.then((db_) => {
-					db = db_;
-					let importer = sync.dir(tmpPath('imports'), tmpPath('library'), db);
-					importer.cancel();
-					return importer.promise;
-				}).then(() => {
-					assert.becomes(db.Photo.count(), 0);
-				});
+		it('should support cancellation', async () => {
+			let db = await setupFixtures();
+			let importer = sync.dir(tmpPath('imports'), tmpPath('library'), db, { move: true });
+			importer.cancel();
+			await importer.promise;
+			let photoCount = await db.Photo.count();
+			assert.equal(photoCount, 0);
 		});
 	});
 });
---
+++ test/unit/moment.js
@@ -0,0 +1,164 @@
+'use strict';
+
+const momentjs = require('moment');
+const path = require('path');
+
+const database = require(libPath('db'));
+
+describe('moment', () => {
+	let db;
+	let Moment;
+	let Photo;
+
+	beforeEach(async () => {
+		await destroyTempDirectory();
+		await makeTempDirectory();
+
+		db = await database(tmpPath('db.sqlite'));
+		await db.migrator.up();
+
+		Moment = db.Moment;
+		Photo = db.Photo;
+	});
+
+	it('should coalesce adjacent moments', async () => {
+		let photo1 = await Photo.create({
+			hash: "1",
+			takenAt: momentjs("2017-01-01")
+		});
+		let moment1 = await photo1.getMoment();
+
+		let start = momentjs(moment1.start);
+		let end = momentjs(moment1.end);
+		assert.ok(momentjs(photo1.takenAt).subtract(Moment.DURATION).isSame(start));
+		assert.ok(momentjs(photo1.takenAt).add(Moment.DURATION).isSame(end));
+
+		let photo2 = await Photo.create({
+			hash: "2",
+			takenAt: momentjs(photo1.takenAt).add(Moment.DURATION)
+		});
+
+		moment1 = await photo1.getMoment();
+		start = momentjs(moment1.start);
+		end = momentjs(moment1.end);
+		assert.ok(momentjs(photo1.takenAt).subtract(Moment.DURATION).isSame(start));
+		assert.ok(momentjs(photo2.takenAt).add(Moment.DURATION).isSame(end));
+
+		// the two photos have the same timestamp, so should coalesce to one moment
+		assert.equal(await Moment.count(), 1);
+
+		let photo3 = await Photo.create({
+			hash: "3",
+			takenAt: momentjs(photo2.takenAt).add(Moment.DURATION)
+		})
+
+		assert.equal(await Moment.count(), 1);
+
+		moment1 = await photo1.getMoment();
+		start = momentjs(moment1.start);
+		end = momentjs(moment1.end);
+		assert.ok(momentjs(photo1.takenAt).subtract(Moment.DURATION).isSame(start));
+		assert.ok(momentjs(photo3.takenAt).add(Moment.DURATION).isSame(end));
+
+		assert.ok(moment1.inRange(photo1.takenAt));
+		assert.ok(moment1.inRange(photo3.takenAt));
+
+		// now add a photo way a bit out of range to create a new moment, and then
+		// create another photo in the middle to coalesce the two connecting moments
+
+		let photo4 = await Photo.create({
+			hash: "4",
+			takenAt: momentjs(photo3.takenAt).add(Moment.DURATION).add(Moment.DURATION)
+		});
+
+		assert.equal(await Moment.count(), 2);
+
+		let photo5 = await Photo.create({
+			hash: "5",
+			takenAt: momentjs(photo3.takenAt).add(Moment.DURATION)
+		});
+
+		assert.equal(await Moment.count(), 1);
+
+		moment1 = await photo1.getMoment();
+		start = momentjs(moment1.start);
+		end = momentjs(moment1.end);
+		assert.ok(momentjs(photo1.takenAt).subtract(Moment.DURATION).isSame(start));
+		assert.ok(momentjs(photo5.takenAt).add(Moment.DURATION).isSame(end));
+
+		// removing photos from the middle should not split the moment once it
+		// has been combined/coalesced
+
+		await Promise.all([
+			photo2.destroy(),
+			photo3.destroy(),
+			photo4.destroy()
+		]);
+
+		assert.equal(await Moment.count(), 1);
+
+		moment1 = await photo1.getMoment();
+		start = momentjs(moment1.start);
+		end = momentjs(moment1.end);
+		assert.ok(momentjs(photo1.takenAt).subtract(Moment.DURATION).isSame(start));
+		assert.ok(momentjs(photo5.takenAt).add(Moment.DURATION).isSame(end));
+	});
+
+	describe('delete', () => {
+		it('should cascade to connected photos', async () => {
+			let moment = await Moment.create({
+				start: momentjs("2017-01-01"),
+				end: momentjs("2017-01-03")
+			});
+
+			let photo1 = await Photo.create({
+				hash: "1",
+				takenAt: momentjs("2017-01-02")
+			});
+
+			let [momentCount, photoCount] = await Promise.all([
+				Moment.count(),
+				Photo.count()
+			]);
+
+			assert.equal(momentCount, 1);
+			assert.equal(photoCount, 1);
+
+			await moment.destroy();
+
+			[momentCount, photoCount] = await Promise.all([
+				Moment.count(),
+				Photo.count()
+			]);
+
+			assert.equal(momentCount, 0);
+			assert.equal(photoCount, 0);
+		});
+	});
+
+	it('should have hooks', async () => {
+		return (async () => {
+			let photo1 = await Photo.create({
+				hash: "1",
+				takenAt: momentjs("2017-01-01")
+			});
+			let photo2 = Photo.build({
+				hash: "2",
+				takenAt: momentjs("2017-01-02")
+			});
+			await photo2.save();
+
+			assert.equal(await Moment.count(), 2);
+			await photo1.destroy();
+			assert.equal(await Moment.count(), 1);
+
+			let moment3 = await Moment.create({
+				start: momentjs(),
+				end: momentjs()
+			});
+			assert.equal(await Moment.count(), 2);
+			await photo2.setMoment(moment3);
+			assert.equal(await Moment.count(), 1);
+		})();
+	});
+});
