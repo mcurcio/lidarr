cb35ddecafe3c34e66af39cc41c3f9b1b1cd7936
Matt Curcio <github@mcurcio.com>
2017-07-19 21:34:55 -0700

    add relay compiler to web pack watch

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,20 @@
 'use strict';
 
+const childProcess = require('child_process');
+const fs = require('fs-extra');
 const path = require('path');
+const webpack = require('webpack');
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+class RelayCompilerPlugin {
+	apply(compiler) {
+		compiler.plugin('before-compile', (compiler, callback) => {
+				const r = path.join(path.resolve(__dirname), "node_modules", "relay-compiler", "bin", "relay-compiler");
+				childProcess.exec(`${r} --src src/ --schema ./schema.json`, callback);
+		});
+	}
+};
 
 module.exports = {
 	devtool: "source-map",
@@ -19,6 +31,8 @@ module.exports = {
 		]
 	},
 	plugins: [
+		new RelayCompilerPlugin(),
+//		new webpack.optimize.UglifyJsPlugin(),
 		new HtmlWebpackPlugin({
 			template: path.resolve(__dirname, "./public/index.html")
 		})
